e897fc0844ed70d9bb5c424fce46c8cca624d015
Grant Guo <gguo@olm-gguo.oicr.on.ca>
2018-01-18 12:31:00 -0500

    add transformation from protobuf to spark sql StructType

---
+++ kf-portal-etl-common/src/main/scala/io/kf/etl/transform/ProtoBuf2StructType.scala
@@ -0,0 +1,36 @@
+package io.kf.etl.transform
+
+import org.apache.spark.sql.types._
+
+import scalapb.descriptors.{Descriptor, FieldDescriptor, ScalaType}
+
+object ProtoBuf2StructType {
+  private def toSparkSQLType(fd:FieldDescriptor): DataType = {
+
+    val basicType: DataType =
+      fd.scalaType match {
+        case ScalaType.Boolean => BooleanType
+        case ScalaType.String => StringType
+        case ScalaType.Int => IntegerType
+        case ScalaType.Long => LongType
+        case ScalaType.Double => DoubleType
+        case ScalaType.Message(descriptor) => parseDescriptor(descriptor)
+        case ScalaType.ByteString => BinaryType
+        case ScalaType.Float => FloatType
+        case _ => StringType
+      }
+
+    if(fd.isRepeated)
+      ArrayType(basicType)
+    else
+      basicType
+  }
+
+  private def parseFieldDescriptor(fd: FieldDescriptor): StructField = {
+    StructField(fd.name, toSparkSQLType(fd), fd.isOptional)
+  }
+
+  def parseDescriptor(descriptor: Descriptor): StructType = {
+    StructType(descriptor.fields.map(parseFieldDescriptor(_)))
+  }
+}
