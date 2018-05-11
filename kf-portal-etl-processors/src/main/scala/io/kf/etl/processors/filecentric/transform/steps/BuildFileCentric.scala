9fad46b456b72dc6ff6f78302128ca72abac8b8b
Grant Guo <gguo@olm-gguo.local>
2018-05-11 11:50:52 -0400

    filter out index files

--- kf-portal-etl-processors/src/main/scala/io/kf/etl/processors/filecentric/transform/steps/BuildFileCentric.scala
+++ kf-portal-etl-processors/src/main/scala/io/kf/etl/processors/filecentric/transform/steps/BuildFileCentric.scala
@@ -129,6 +129,13 @@ class BuildFileCentric(override val ctx: StepContext) extends StepExecutable[Dat
           }
         )
 
+    }).filter(fc => {
+      fc.dataType match {
+        case Some(data_type) => {
+          !data_type.toLowerCase.split(' ').takeRight(1)(0).equals("index")
+        }
+        case None => true
+      }
     })
   }
 }
