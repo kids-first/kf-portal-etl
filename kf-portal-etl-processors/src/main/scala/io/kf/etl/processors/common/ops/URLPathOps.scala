a8be884cdf25a7d3b60f342722d3a9d649237411
Grant Guo <gguo@olm-gguo.oicr.on.ca>
2018-05-25 15:00:10 -0400

    to delete s3 directory, should list objects in the bucket first

--- kf-portal-etl-processors/src/main/scala/io/kf/etl/processors/common/ops/URLPathOps.scala
+++ kf-portal-etl-processors/src/main/scala/io/kf/etl/processors/common/ops/URLPathOps.scala
@@ -4,8 +4,11 @@ import java.io.File
 import java.net.URL
 
 import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.model.DeleteObjectsRequest
 import org.apache.commons.io.FileUtils
 import org.apache.hadoop.fs.{FileSystem, Path}
+
+import scala.collection.convert.WrapAsScala
 
 object URLPathOps {
   def removePathIfExists(path: URL)(implicit hdfs: FileSystem, s3Client: AmazonS3):Unit = {
@@ -45,7 +48,16 @@ object URLPathOps {
 
   private class S3URLOps(s3:AmazonS3) extends URLOps {
     override def removePathIfExists(path: URL): Unit = {
-      s3.deleteObject(path.getHost, path.getPath)
+      val bucket = path.getHost
+      val common_path = path.getPath.split('/').filter(!_.trim.isEmpty).mkString("", "/", "/")
+
+      val deleteObjsReq = new DeleteObjectsRequest(bucket).withKeys(
+        WrapAsScala.asScalaBuffer( s3.listObjects(bucket).getObjectSummaries ).filter(summary => {
+          summary.getKey.startsWith(common_path)
+        }).map(_.getKey) : _*
+      )
+
+      s3.deleteObjects(deleteObjsReq)
     }
   }
 }
