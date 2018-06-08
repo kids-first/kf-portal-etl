7e3bd82eaadae7cb713fc4512ce405e9217f0665
Grant Guo <gguo@olm-gguo.oicr.on.ca>
2018-06-08 16:07:11 -0400

    update docker README.md

--- docker/etl/kf-etl-submit.sh
+++ docker/etl/kf-etl-submit.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 service ssh start
-sleep 5
+
+while [ $(ps axu|grep sshd|grep -v grep|wc -l) -eq 0 ]
+do
+    echo wait for 5s
+    sleep 5s
+done
+
+ps aux|grep sshd
+
 . /kf-etl/spark/sbin/start-all.sh
 
-    KF_ETL_CONFIG="-Dkf.etl.config=${ETL_CONF_URL}"
+KF_ETL_CONFIG="-Dkf.etl.config=${ETL_CONF_URL}"
 
 /kf-etl/spark/bin/spark-submit --master ${SPARK_MASTER} --deploy-mode ${SPARK_DEPLOY_MODE} --class io.kf.etl.ETLMain --driver-java-options "${KF_ETL_CONFIG}" --conf "spark.executor.extraJavaOptions=${KF_ETL_CONFIG}" ${ETL_JAR_URL}
 
