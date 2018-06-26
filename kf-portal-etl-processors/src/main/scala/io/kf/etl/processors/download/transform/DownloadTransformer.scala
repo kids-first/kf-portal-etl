8afd0dee459f0ac4da691f2b91a5fa7e10c7d1ed
Grant Guo <gguo@olm-gguo.oicr.on.ca>
2018-06-26 09:41:17 -0400

    update tailrec function

--- kf-portal-etl-processors/src/main/scala/io/kf/etl/processors/download/transform/DownloadTransformer.scala
+++ kf-portal-etl-processors/src/main/scala/io/kf/etl/processors/download/transform/DownloadTransformer.scala
@@ -15,32 +15,32 @@ class DownloadTransformer(val context:DownloadContext) {
     val dataset =
       EntityDataSet(
         participants = {
-          endpoints.map(ees => context.appContext.sparkSession.createDataset(retrieval.retrieve1[EParticipant](Some(ees.participants), Seq.empty))).foldLeft(context.appContext.sparkSession.emptyDataset[EParticipant]){
+          endpoints.map(ees => context.appContext.sparkSession.createDataset(retrieval.retrieve[EParticipant](Some(ees.participants), Seq.empty))).foldLeft(context.appContext.sparkSession.emptyDataset[EParticipant]){
             (left, right) => left.union(right)
           }.cache()
         },
         families = {
-          endpoints.map(ees => context.appContext.sparkSession.createDataset(retrieval.retrieve1[EFamily](Some(ees.families), Seq.empty))).foldLeft(context.appContext.sparkSession.emptyDataset[EFamily]){
+          endpoints.map(ees => context.appContext.sparkSession.createDataset(retrieval.retrieve[EFamily](Some(ees.families), Seq.empty))).foldLeft(context.appContext.sparkSession.emptyDataset[EFamily]){
             (left, right) => left.union(right)
           }.cache()
         },
         biospecimens = {
-          endpoints.map(ees => context.appContext.sparkSession.createDataset(retrieval.retrieve1[EBiospecimen](Some(ees.biospecimens), Seq.empty))).foldLeft(context.appContext.sparkSession.emptyDataset[EBiospecimen]){
+          endpoints.map(ees => context.appContext.sparkSession.createDataset(retrieval.retrieve[EBiospecimen](Some(ees.biospecimens), Seq.empty))).foldLeft(context.appContext.sparkSession.emptyDataset[EBiospecimen]){
             (left, right) => left.union(right)
           }.cache()
         },
         diagnoses = {
-          endpoints.map(ees => context.appContext.sparkSession.createDataset(retrieval.retrieve1[EDiagnosis](Some(ees.diagnoses), Seq.empty))).foldLeft(context.appContext.sparkSession.emptyDataset[EDiagnosis]){
+          endpoints.map(ees => context.appContext.sparkSession.createDataset(retrieval.retrieve[EDiagnosis](Some(ees.diagnoses), Seq.empty))).foldLeft(context.appContext.sparkSession.emptyDataset[EDiagnosis]){
             (left, right) => left.union(right)
           }.cache()
         },
         familyRelationships = {
-          endpoints.map(ees => context.appContext.sparkSession.createDataset(retrieval.retrieve1[EFamilyRelationship](Some(ees.familyRelationships), Seq.empty))).foldLeft(context.appContext.sparkSession.emptyDataset[EFamilyRelationship]){
+          endpoints.map(ees => context.appContext.sparkSession.createDataset(retrieval.retrieve[EFamilyRelationship](Some(ees.familyRelationships), Seq.empty))).foldLeft(context.appContext.sparkSession.emptyDataset[EFamilyRelationship]){
             (left, right) => left.union(right)
           }.cache()
         },
         genomicFiles = {
-          endpoints.map(ees => context.appContext.sparkSession.createDataset(retrieval.retrieve1[EGenomicFile](Some(ees.genomicFiles), Seq.empty))).foldLeft(context.appContext.sparkSession.emptyDataset[EGenomicFile]){
+          endpoints.map(ees => context.appContext.sparkSession.createDataset(retrieval.retrieve[EGenomicFile](Some(ees.genomicFiles), Seq.empty))).foldLeft(context.appContext.sparkSession.emptyDataset[EGenomicFile]){
             (left, right) => left.union(right)
           }.filter(f => {
             f.dataType match {
@@ -52,27 +52,27 @@ class DownloadTransformer(val context:DownloadContext) {
           }).cache()
         },
         investigators = {
-          endpoints.map(ees => context.appContext.sparkSession.createDataset(retrieval.retrieve1[EInvestigator](Some(ees.investigators), Seq.empty))).foldLeft(context.appContext.sparkSession.emptyDataset[EInvestigator]){
+          endpoints.map(ees => context.appContext.sparkSession.createDataset(retrieval.retrieve[EInvestigator](Some(ees.investigators), Seq.empty))).foldLeft(context.appContext.sparkSession.emptyDataset[EInvestigator]){
             (left, right) => left.union(right)
           }.cache()
         },
         outcomes = {
-          endpoints.map(ees => context.appContext.sparkSession.createDataset(retrieval.retrieve1[EOutcome](Some(ees.outcomes), Seq.empty))).foldLeft(context.appContext.sparkSession.emptyDataset[EOutcome]){
+          endpoints.map(ees => context.appContext.sparkSession.createDataset(retrieval.retrieve[EOutcome](Some(ees.outcomes), Seq.empty))).foldLeft(context.appContext.sparkSession.emptyDataset[EOutcome]){
             (left, right) => left.union(right)
           }.cache()
         },
         phenotypes = {
-          endpoints.map(ees => context.appContext.sparkSession.createDataset(retrieval.retrieve1[EPhenotype](Some(ees.phenotypes), Seq.empty))).foldLeft(context.appContext.sparkSession.emptyDataset[EPhenotype]){
+          endpoints.map(ees => context.appContext.sparkSession.createDataset(retrieval.retrieve[EPhenotype](Some(ees.phenotypes), Seq.empty))).foldLeft(context.appContext.sparkSession.emptyDataset[EPhenotype]){
             (left, right) => left.union(right)
           }.cache()
         },
         sequencingExperiments = {
-          endpoints.map(ees => context.appContext.sparkSession.createDataset(retrieval.retrieve1[ESequencingExperiment](Some(ees.sequencingExperiments), Seq.empty))).foldLeft(context.appContext.sparkSession.emptyDataset[ESequencingExperiment]){
+          endpoints.map(ees => context.appContext.sparkSession.createDataset(retrieval.retrieve[ESequencingExperiment](Some(ees.sequencingExperiments), Seq.empty))).foldLeft(context.appContext.sparkSession.emptyDataset[ESequencingExperiment]){
             (left, right) => left.union(right)
           }.cache()
         },
         studies = {
-          endpoints.map(ees => context.appContext.sparkSession.createDataset(retrieval.retrieve1[EStudy](Some(ees.studies), Seq.empty))).foldLeft(context.appContext.sparkSession.emptyDataset[EStudy]){
+          endpoints.map(ees => context.appContext.sparkSession.createDataset(retrieval.retrieve[EStudy](Some(ees.studies), Seq.empty))).foldLeft(context.appContext.sparkSession.emptyDataset[EStudy]){
             (left, right) => left.union(right)
           }.cache()
         },
