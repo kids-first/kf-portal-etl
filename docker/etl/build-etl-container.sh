324c241ea5eebd2b50b00cd740548057dd456e85
rtisma <rtisma@gmail.com>
2018-07-24 21:17:52 -0400

    updated

--- docker/etl/build-etl-container.sh
+++ docker/etl/build-etl-container.sh
@@ -4,4 +4,4 @@ imagename="kids-first/etl"
 imageversion="0.0.1"
 sparkmaster="spark://localhost:7077"
 
-sudo docker build --build-arg SPARK_VERSION=2.3.0 --build-arg SPARK_MASTER=${sparkmaster} -t ${imagename}:${imageversion} .
+docker build  --build-arg SPARK_MASTER=${sparkmaster} -t ${imagename}:${imageversion} .
--- docker/etl/run-etl-container.sh
+++ docker/etl/run-etl-container.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+host_jar_location="../../kf-portal-etl-pipeline/target/scala-2.11/kf-portal-etl.jar"
+host_conf_location="../kf_etl.conf"
 containername="kf-etl"
 imagename="kids-first/etl"
 imageversion="0.0.1"
+ports=( 4040 6066 7077 8080 8081 )
+port_cmd=""
+for port in ${ports[*]}; do
+    port_cmd+=" -p ${port}:${port} "
+done
 
-sudo docker run --hostname $containername --name $containername ${imagename}:${imageversion}
+docker run \
+    --rm \
+    -v "${host_jar_location}:/kf-etl/mnt/lib/kf-portal-etl.jar" \
+    -v "${host_conf_location}:/kf-etl/mnt/conf/kf_etl.conf" \
+    ${port_cmd} \
+    --name $containername \
+    ${imagename}:${imageversion}
 
