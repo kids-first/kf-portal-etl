10e9303a39ea58b08e8a2b3ca7121a3ae4bb6a6b
rtisma <rtisma@gmail.com>
2018-07-25 20:34:39 -0400

    updated

--- docker/etl/run-etl-container.sh
+++ docker/etl/run-etl-container.sh
@@ -7,12 +7,18 @@ imagename="kids-first/etl"
 imageversion="0.0.1"
 ports=( 4040 6066 7077 8080 8081 )
 port_cmd=""
+kf_study_id="SD_9PYZAHHE"
+kf_release_id="RE_1J6FQHFM"
+network="default"
 for port in ${ports[*]}; do
     port_cmd+=" -p ${port}:${port} "
 done
 
 docker run \
     --rm \
+    -e KF_STUDY_ID=${kf_study_id} \
+    -e KF_RELEASE_ID=${kf_release_id} \
+    --net=${network} \
     -v "${host_jar_location}:/kf-etl/mnt/lib/kf-portal-etl.jar" \
     -v "${host_conf_location}:/kf-etl/mnt/conf/kf_etl.conf" \
     ${port_cmd} \
