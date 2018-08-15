391e51906a98f92f37755b73b29aed3f909481ca
Jon Eubank <joneubank@gmail.com>
2018-08-15 11:58:38 -0400

    Family Composition labelling corrected for most cases (except proband-only)

--- kf-portal-etl-processors/src/main/scala/io/kf/etl/processors/common/step/impl/MergeFamily.scala
+++ kf-portal-etl-processors/src/main/scala/io/kf/etl/processors/common/step/impl/MergeFamily.scala
@@ -11,71 +11,28 @@ import org.apache.spark.sql.Dataset
 
 class MergeFamily(override val ctx: StepContext) extends StepExecutable[Dataset[Participant_ES], Dataset[Participant_ES]] {
   override def process(participants: Dataset[Participant_ES]): Dataset[Participant_ES] = {
-
     import ctx.spark.implicits._
-    val availableDataTypes_broadcast = calculcateAvailableDataTypes(ctx.entityDataset)
 
-    /**
-      * flattenedFamilyRelationship is a map
-      * key  : participant id
-      * value: list of relationship which the current participant holds in the whole family
-      */
-    val flattenedFamilyRelationship =
-      ctx.spark.sparkContext.broadcast(
-        ctx.entityDataset.familyRelationships.map(fr => {
-          fr.copy(
-            participant1ToParticipant2Relation = Some(fr.participant2ToParticipant1Relation.get.toLowerCase),
-            participant2ToParticipant1Relation = Some(fr.participant1ToParticipant2Relation.get.toLowerCase)
-          )
-        }).flatMap(tf => {
-          Seq(
-            tf,
-            EFamilyRelationship(
-              kfId = tf.kfId,
-              createdAt = tf.createdAt,
-              modifiedAt = tf.modifiedAt,
-              participant1 = tf.participant2,
-              participant2 = tf.participant1,
-              participant2ToParticipant1Relation = tf.participant1ToParticipant2Relation,
-              participant1ToParticipant2Relation = tf.participant2ToParticipant1Relation
-            )
-          )
-        }).groupByKey(tf => tf.participant1.get)
-          .mapGroups((participant_id, iterator) => {
-            (
-              participant_id,
-              iterator.collect{
-                case tf: EFamilyRelationship if(tf.participant1ToParticipant2Relation.isDefined) => {
-                  tf.participant1ToParticipant2Relation.get
-                }
-              }.toSeq
-            )
-          })
-          .collect().groupBy(_._1).map(tuple => {
-            (
-              tuple._1,
-              tuple._2.flatMap(_._2)
-            )
-          })
-          .map(tuple => (tuple._1, tuple._2.toSet))
-      )
+    val availableDataTypesBroadcast = calculateAvailableDataTypes(ctx.entityDataset)
+    val flattenedFamilyRelationshipBroadcast = getFlattenedFamilyRelationship(ctx.entityDataset)
 
-    participants.groupByKey(_.familyId).flatMapGroups((family_id, iterator) => {
-      MergeFamily.deduceFamilyCompositions(family_id, iterator.toSeq, flattenedFamilyRelationship, availableDataTypes_broadcast)
-    })
-
+    participants
+      .groupByKey(_.familyId)
+      .flatMapGroups((family_id, iterator) => {
+        MergeFamily.deduceFamilyCompositions(family_id, iterator.toSeq, flattenedFamilyRelationshipBroadcast, availableDataTypesBroadcast)
+      })
   }
 
-  private def calculcateAvailableDataTypes(data: EntityDataSet): Broadcast[Map[String, Seq[String]]] = {
+  private def calculateAvailableDataTypes(entityDataset: EntityDataSet): Broadcast[Map[String, Seq[String]]] = {
     import ctx.spark.implicits._
 
     val par_bio =
-      data.participants.joinWith(data.biospecimens, data.participants.col("kfId") === data.biospecimens.col("participantId")).map(tuple => {
+      entityDataset.participants.joinWith(entityDataset.biospecimens, entityDataset.participants.col("kfId") === entityDataset.biospecimens.col("participantId")).map(tuple => {
         ParticipantId_BiospecimenId(parId = tuple._1.kfId.get, bioId = tuple._2.kfId.get)
       })
 
     ctx.spark.sparkContext.broadcast[Map[String, Seq[String]]](
-      par_bio.joinWith(data.genomicFiles, par_bio.col("bioId") ===  data.genomicFiles.col("biospecimenId")).groupByKey(tuple => {
+      par_bio.joinWith(entityDataset.genomicFiles, par_bio.col("bioId") ===  entityDataset.genomicFiles.col("biospecimenId")).groupByKey(tuple => {
         tuple._1.parId
       }).mapGroups((parId, iterator) => {
         val seq = iterator.toSeq
@@ -90,10 +47,76 @@ class MergeFamily(override val ctx: StepContext) extends StepExecutable[Dataset[
       }).toMap
     )
   }
+
+  private def getFlattenedFamilyRelationship(entityDataset: EntityDataSet): Broadcast[Map[String, Set[String]]] = {
+    /**
+      * flattenedFamilyRelationship is a map
+      * key  : participant id
+      * value: Set of relation types as lowercase strings
+      */
+    import ctx.spark.implicits._
+
+    ctx.spark.sparkContext.broadcast(
+      entityDataset.familyRelationships
+
+        // Make a copy of every family relationship so we have an instance with each family member as participant 1
+        .flatMap(tf => {
+          Seq(
+            tf,
+            EFamilyRelationship(
+              kfId = tf.kfId,
+              createdAt = tf.createdAt,
+              modifiedAt = tf.modifiedAt,
+              participant1 = tf.participant2,
+              participant2 = tf.participant1,
+              participant2ToParticipant1Relation = tf.participant1ToParticipant2Relation,
+              participant1ToParticipant2Relation = tf.participant2ToParticipant1Relation
+            )
+          )
+        })
+
+        // group the relationship objects for each participant
+        .groupByKey(_.participant1.get)
+
+        // for each group, create a tuple of form (participant_id, Seq(relation type))
+        // This also applies the toLowerCase modifier to the relationship text
+        .mapGroups((participant_id, iterator) => {
+          (
+            participant_id,
+            iterator.collect{
+              case tf: EFamilyRelationship if tf.participant1ToParticipant2Relation.isDefined => {
+                tf.participant1ToParticipant2Relation.get.toLowerCase
+              }
+            }.toSet
+          )
+        })
+        .collect()
+
+        // Uncertain why this groupBy is called, shouldn't we already have the groups correct after our mapGroups above?
+        .groupBy(_._1)
+
+        // Flatten the tuple._2 into a list of relation types
+        .map(tuple => {
+          (
+            tuple._1,
+            tuple._2.flatMap(_._2)
+          )
+        })
+
+        // Call toSet on the tuple values to remove duplicates
+        .map(tuple => (tuple._1, tuple._2.toSet))
+    )
+  }
 }
 
 object MergeFamily {
-  case class FamilyStructure(father: Option[Participant_ES] = None, mother: Option[Participant_ES] = None, proband: Option[Participant_ES] = None, others: Seq[(String, Participant_ES)] = Seq.empty)
+
+  case class FamilyStructure( father: Option[Participant_ES] = None,
+                              mother: Option[Participant_ES] = None,
+                              proband: Option[Participant_ES] = None,
+                              others: Seq[(String, Participant_ES)] = Seq.empty
+                            )
+
   class ProbandMissingInFamilyException extends Exception("Family has no proband child!")
   /*
     familyRelationship map:
@@ -101,30 +124,39 @@ object MergeFamily {
     value: list of relationship in the family, for example, father, mother, child
    */
 
-  def deduceFamilyCompositions(familyId:Option[String], family: Seq[Participant_ES], familyRelationship_broadcast: Broadcast[Map[String, Set[String]]], availableDataTypes_broadcast: Broadcast[Map[String, Seq[String]]]): Seq[Participant_ES] = {
-
+  def deduceFamilyCompositions( familyId: Option[String],
+                                family: Seq[Participant_ES],
+                                familyRelationship_broadcast: Broadcast[Map[String, Set[String]]],
+                                availableDataTypes_broadcast: Broadcast[Map[String, Seq[String]]]
+                              ): Seq[Participant_ES] =
+  {
     val familyRelationship = familyRelationship_broadcast.value
     val mapOfAvailableDataTypes = availableDataTypes_broadcast.value
 
     familyId match {
       case None => family
+
       case Some(id) => {
         val familyStructure =
-          family.foldLeft(FamilyStructure()){(family_structure, participant) => {
+          family
+            .foldLeft(FamilyStructure()) { (family_structure, participant) => {
+
             familyRelationship.get(participant.kfId.get) match {
               case None => {
                 participant.isProband match {
-                  case Some(isProband) if isProband == true => {
+                  case Some(isProband) if isProband => {
                     family_structure.copy(proband = Some(participant))
                   }
                   case _ => {
                     family_structure.copy(
-                      others = (family_structure.others :+ ("member", participant))
+                      others = family_structure.others :+ ("member", participant)
                     )
                   }
                 }
               }
+
               case Some(relationships) => {
+
                 if(relationships.contains("father")) {
                   family_structure.father match {
                     case Some(_) => {
@@ -135,6 +167,7 @@ object MergeFamily {
                     case None => family_structure.copy(father = Some(participant))
                   }
                 }
+
                 else if(relationships.contains("mother")) {
                   family_structure.mother match {
                     case Some(_) => {
@@ -146,11 +179,12 @@ object MergeFamily {
                   }
 
                 }
+
                 else if(relationships.contains("child")) {
                   if (participant.isProband.isDefined && participant.isProband.get)
                     family_structure.copy(proband =Some(participant))
                   else
-                    family_structure.copy(others = (family_structure.others :+ ("child", participant)))
+                    family_structure.copy(others = family_structure.others :+ ("child", participant))
                 }
                 else {
                   family_structure.copy(
@@ -162,6 +196,7 @@ object MergeFamily {
           }}// end of family.foldLeft
 
         familyStructure match {
+          // trio = mother, father, proband
           case FamilyStructure(Some(father), Some(mother), Some(proband), Seq()) => {
             val sharedHpoIds = getSharedHpoIds(Seq(father, mother, proband))
             val family_availableDataTypes = getAvailableDataTypes(Seq(father, mother, proband), mapOfAvailableDataTypes)
@@ -212,11 +247,13 @@ object MergeFamily {
             )
 
           }//end of case FamilyStructure(Some(father), Some(mother), Some(proband), Seq())
+
+          // trio+ = mother, father, proband, other
           case FamilyStructure(Some(father), Some(mother), Some(proband), Seq(head, tail @ _*)) => {
 
             val members = Seq(father, mother, proband, head._2) ++ tail.map(_._2)
             val sharedHpoIds = getSharedHpoIds(members)
-            val family_availableDataTypes = getAvailableDataTypes(members, mapOfAvailableDataTypes).toSet.toSeq
+            val family_availableDataTypes = getAvailableDataTypes(members, mapOfAvailableDataTypes).distinct
 
             val composition =
               FamilyComposition_ES(
@@ -277,6 +314,8 @@ object MergeFamily {
               )
             })
           }//end of case FamilyStructure(Some(father), Some(mother), Some(proband), Seq(head, tail @ _*))
+
+          // duo = father, proband
           case FamilyStructure(Some(father), None, Some(proband), Seq()) => {
             val sharedHpoIds = getSharedHpoIds(Seq(father, proband))
             val family_availableDataTypes = getAvailableDataTypes(Seq(father, proband), mapOfAvailableDataTypes)
@@ -314,6 +353,8 @@ object MergeFamily {
             )
 
           }//end of case FamilyStructure(Some(father), None, Some(proband), seq)
+
+          // duo+ = father, proband, other
           case FamilyStructure(Some(father), None, Some(proband), Seq(head, tail @ _*)) => {
             val members = Seq(father, proband, head._2) ++ tail.map(_._2)
             val sharedHpoIds = getSharedHpoIds(members)
@@ -364,6 +405,8 @@ object MergeFamily {
               )
             })
           }//end of case FamilyStructure(Some(father), None, Some(proband), seq)
+
+          // duo = mother, proband
           case FamilyStructure(None, Some(mother), Some(proband), Seq()) => {
             val sharedHpoIds = getSharedHpoIds(Seq(mother, proband))
             val family_availableDataTypes = getAvailableDataTypes(Seq(mother, proband), mapOfAvailableDataTypes)
@@ -401,6 +444,8 @@ object MergeFamily {
             )
 
           }//end of case FamilyStructure(None, Some(mother), Some(proband), seq)
+
+          // duo+ = mother, proband, other
           case FamilyStructure(None, Some(mother), Some(proband), Seq(head, tail @ _*)) => {
             val members = Seq(mother, proband, head._2) ++ tail.map(_._2)
             val sharedHpoIds = getSharedHpoIds(members)
@@ -451,7 +496,11 @@ object MergeFamily {
               )
             })
           }//end of case FamilyStructure(None, Some(mother), Some(proband), seq)
+
+          // proband-only = proband
           case FamilyStructure(None, None, Some(proband), Seq()) => {
+            println(s"PROBAND-ONLY familyStructure: ${familyStructure}")
+
             val members = Seq(proband)
             val sharedHpoIds = getSharedHpoIds(members)
             val family_availableDataTypes = getAvailableDataTypes(members, mapOfAvailableDataTypes)
@@ -478,7 +527,11 @@ object MergeFamily {
               )
             )
           }//end of FamilyStructure(None, None, Some(proband), Seq())
+
+          // other = proband, other
           case FamilyStructure(None, None, Some(proband), Seq(head, tail @ _*)) => {
+            println(s"OTHER familyStructure: ${familyStructure}")
+
             val members = Seq(proband, head._2) ++ tail.map(_._2)
             val sharedHpoIds = getSharedHpoIds(members)
             val family_availableDataTypes = getAvailableDataTypes(members, mapOfAvailableDataTypes)
@@ -517,7 +570,10 @@ object MergeFamily {
               )
             })
           }//end of case FamilyStructure(None, None, Some(proband), seq)
+
+          // other = default (fall through)
           case _ => {
+            println(s"DEFAULT familyStructure: ${familyStructure}")
 
             val sharedHpoIds = getSharedHpoIds(family)
             val family_availableDataTypes = getAvailableDataTypes(family, mapOfAvailableDataTypes)
@@ -551,14 +607,21 @@ object MergeFamily {
     }//end of familyId match
   }
 
-  def getFamilyMemberFromParticipant(participant: Participant_ES, relationship:String, familySharedHpoIds:Seq[String], mapOfAvailableDataTypes: Map[String, Seq[String]]): FamilyMember_ES = {
+  def getFamilyMemberFromParticipant( participant: Participant_ES,
+                                      relationship:String,
+                                      familySharedHpoIds:Seq[String],
+                                      mapOfAvailableDataTypes: Map[String, Seq[String]]): FamilyMember_ES =
+  {
+
+    val availableDataTypes = mapOfAvailableDataTypes.get(participant.kfId.get) match {
+      case Some(types) => types.toSet.toSeq
+      case None => Seq.empty
+    }
+
     FamilyMember_ES(
       kfId = participant.kfId,
       isProband = participant.isProband,
-      availableDataTypes = mapOfAvailableDataTypes.get(participant.kfId.get) match {
-        case Some(types) => types.toSet.toSeq
-        case None => Seq.empty
-      },
+      availableDataTypes = availableDataTypes,
       phenotype = participant.phenotype,
       race = participant.race,
       ethnicity = participant.ethnicity,
@@ -585,7 +648,7 @@ object MergeFamily {
 
     seqOfDataTypes.tail.foldLeft(seqOfDataTypes.head){(left, right) => {
       left.intersect(right)
-    }}.toSet.toSeq
+    }}.distinct
   }
 
   def getSharedHpoIds(participants: Seq[Participant_ES]): Seq[String] = {
@@ -602,7 +665,7 @@ object MergeFamily {
       participant.phenotype.flatMap(pt => {
         pt.hpo match {
           case None => None
-          case (Some(hpo)) => {
+          case Some(hpo) => {
             Some(hpo.hpoPhenotypeObserved)
           }
         }
