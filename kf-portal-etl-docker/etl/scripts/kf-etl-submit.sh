38889e67136e8415cc21be0e87e8bccbbf7cf6ea
Jeremy Costanza <jecos.crsj@gmail.com>
2019-05-01 13:26:06 -0400

    Stop using a spark cluster, run on local

--- kf-portal-etl-docker/etl/scripts/kf-etl-submit.sh
+++ kf-portal-etl-docker/etl/scripts/kf-etl-submit.sh
@@ -40,9 +40,7 @@ done
 
 ps aux|grep sshd
 
-. /kf-etl/spark/sbin/start-all.sh
-
 KF_ETL_CONFIG="-Dkf.etl.config=${ETL_CONF_URL}"
 
-/kf-etl/spark/bin/spark-submit --master ${SPARK_MASTER} --deploy-mode ${SPARK_DEPLOY_MODE} --class io.kf.etl.ETLMain --driver-java-options "${KF_ETL_CONFIG}" --conf "spark.executor.extraJavaOptions=${KF_ETL_CONFIG}" ${ETL_JAR_URL} -study_id  ${KF_STUDY_ID} -release_id ${KF_RELEASE_ID}
+/kf-etl/spark/bin/spark-submit --master local[*] --class io.kf.etl.ETLMain --driver-java-options "${KF_ETL_CONFIG}" --conf "spark.executor.extraJavaOptions=${KF_ETL_CONFIG}" ${ETL_JAR_URL} -study_id  ${KF_STUDY_ID} -release_id ${KF_RELEASE_ID}
 
