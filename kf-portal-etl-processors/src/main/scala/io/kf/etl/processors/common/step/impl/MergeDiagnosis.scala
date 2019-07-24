e465c3377dc80b30a1ea7e6b9e99197eb1881ea1
jecos <jecos.crsj@gmail.com>
2019-07-24 11:11:19 -0400

    Enrich diagnosis upstream, for joining biospecimen with diagnoses

--- kf-portal-etl-processors/src/main/scala/io/kf/etl/processors/common/step/impl/MergeDiagnosis.scala
+++ kf-portal-etl-processors/src/main/scala/io/kf/etl/processors/common/step/impl/MergeDiagnosis.scala
@@ -2,29 +2,21 @@ package io.kf.etl.processors.common.step.impl
 
 import io.kf.etl.es.models.{Diagnosis_ES, Participant_ES}
 import io.kf.etl.external.dataservice.entity.EDiagnosis
-import io.kf.etl.external.hpo.OntologyTerm
 import io.kf.etl.processors.common.converter.PBEntityConverter
 import io.kf.etl.processors.common.step.StepExecutable
 import io.kf.etl.processors.common.step.context.StepContext
-import io.kf.etl.processors.common.step.impl.MergeDiagnosis._
 import org.apache.spark.sql.Dataset
 
 class MergeDiagnosis(override val ctx: StepContext) extends StepExecutable[Dataset[Participant_ES], Dataset[Participant_ES]] {
 
   override def process(participants: Dataset[Participant_ES]): Dataset[Participant_ES] = {
+    import ctx.entityDataset.diagnoses
     import ctx.spark.implicits._
-
-    val enrichedDiagnoses = joinDiagnosisAndOntologyTerms()
-      .map {
-        case (d, optMondoTerm, optNcitTerm) if optMondoTerm.isDefined => d.copy(diagnosisText = optMondoTerm.map(_.name), mondoIdDiagnosis = formatTerm(optMondoTerm), ncitIdDiagnosis = formatTerm(optNcitTerm))
-        case (d, _, optNcitTerm) if optNcitTerm.isDefined => d.copy(diagnosisText = optNcitTerm.map(_.name), mondoIdDiagnosis = None, ncitIdDiagnosis = formatTerm(optNcitTerm))
-        case (d, _, _) => d.copy(diagnosisText = d.sourceTextDiagnosis)
-      }
 
     participants
       .joinWith(
-        enrichedDiagnoses,
-        participants.col("kfId") === enrichedDiagnoses.col("participantId"),
+        diagnoses,
+        participants.col("kfId") === diagnoses.col("participantId"),
         "left_outer"
       )
       .as[(Participant_ES, Option[EDiagnosis])]
@@ -39,23 +31,5 @@ class MergeDiagnosis(override val ctx: StepContext) extends StepExecutable[Datas
       })
   }
 
-  private def joinDiagnosisAndOntologyTerms(): Dataset[(EDiagnosis, Option[OntologyTerm], Option[OntologyTerm])] = {
-    import ctx.entityDataset.diagnoses
-    import ctx.entityDataset.ontologyData.{mondoTerms, ncitTerms}
-    import ctx.spark.implicits._
-
-    val t: Dataset[(EDiagnosis, Option[OntologyTerm], Option[OntologyTerm])] = diagnoses
-      .joinWith(mondoTerms, diagnoses("mondoIdDiagnosis") === mondoTerms("id"), "left_outer")
-      .as[(EDiagnosis, Option[OntologyTerm])]
-      .joinWith(ncitTerms, $"_1.ncitIdDiagnosis" === ncitTerms("id"), "left_outer")
-      .map { case ((d, m), n) => (d, m, Option(n)) }
-    t
-  }
-
-
-}
-
-object MergeDiagnosis{
-  def formatTerm(term: Option[OntologyTerm]): Option[String] = term.map(t => s"${t.name} (${t.id})")
 
 }
--- kf-portal-etl-processors/src/test/scala/io/kf/etl/processors/common/step/impl/MergeDiagnosisTest.scala
+++ kf-portal-etl-processors/src/test/scala/io/kf/etl/processors/common/step/impl/MergeDiagnosisTest.scala
@@ -12,54 +12,39 @@ class MergeDiagnosisTest extends FlatSpec with Matchers with WithSparkSession {
 
   import spark.implicits._
 
-  "process" should "merge diagnoses and participnt and enrich diagnosis, mondo_diagnosis nd ncit diagnosis" in {
+  "process" should "merge diagnoses and participnt" in {
     val p1 = Participant_ES(kfId = Some("participant_id_1"))
-    val diagnoses11 = EDiagnosis(kfId = Some("diagnosis_11"), participantId = Some("participant_id_1"), mondoIdDiagnosis = Some("MONDO:0005072"), ncitIdDiagnosis = Some("NCIT:C0475358"), sourceTextDiagnosis = Some("Neuroblastoma source text"))
-    val diagnoses12 = EDiagnosis(kfId = Some("diagnosis_12"), participantId = Some("participant_id_1"), ncitIdDiagnosis = Some("NCIT:C0475358"), sourceTextDiagnosis = Some("Neuroblastoma source text"))
-    val diagnoses13 = EDiagnosis(kfId = Some("diagnosis_13"), participantId = Some("participant_id_1"), sourceTextDiagnosis = Some("Neuroblastoma source text"))
+    val diagnoses11 = EDiagnosis(kfId = Some("diagnosis_11"), participantId = Some("participant_id_1"))
+    val diagnoses12 = EDiagnosis(kfId = Some("diagnosis_12"), participantId = Some("participant_id_1"))
 
 
     val p2 = Participant_ES(kfId = Some("participant_id_2"))
-    val diagnoses21 = EDiagnosis(kfId = Some("diagnosis_21"), participantId = Some("participant_id_2"), mondoIdDiagnosis = Some("MONDO:0005072"))
-    val diagnoses22 = EDiagnosis(kfId = Some("diagnosis_22"), participantId = Some("participant_id_2"), mondoIdDiagnosis = Some("MONDO:0005073"))
+    val diagnoses21 = EDiagnosis(kfId = Some("diagnosis_21"), participantId = Some("participant_id_2"))
 
     val diagnoses3 = EDiagnosis(kfId = Some("diagnosis_3"))
 
     val p3 = Participant_ES(kfId = Some("participant_id_3"))
 
-    val ontologiesDataset = buildOntologiesDataSet(
-      ncitTerms = Seq(
-        OntologyTerm(name = "Neuroblastoma NCIT", id = "NCIT:C0475358"),
-        OntologyTerm(name = "Ewing Sarcoma NCIT", id = "NCIT:C14165")
-      ),
-      mondoTerms = Seq(
-        OntologyTerm(name = "Neuroblastoma Mondo", id = "MONDO:0005072"),
-        OntologyTerm(name = "Ewing Sarcoma Mondo", id = "MONDO:0005073")
-      )
-
-    )
 
     val entityDataset = buildEntityDataSet(
-      diagnoses = Seq(diagnoses11, diagnoses12, diagnoses13, diagnoses21, diagnoses22, diagnoses3),
-      ontologyData = Some(ontologiesDataset)
+      diagnoses = Seq(diagnoses11, diagnoses12, diagnoses21, diagnoses3)
     )
 
     val merge = new MergeDiagnosis(ctx = buildContext(entityDataset))
 
     val result = merge.process(Seq(p1, p2, p3).toDS()).collect()
 
-    result should contain theSameElementsAs Seq(
+    //We sort diagnoses for each participant
+    val sortedResult = result.map(r => r.copy(diagnoses = r.diagnoses.sortBy(_.kfId)))
+    sortedResult should contain theSameElementsAs Seq(
       Participant_ES(kfId = Some("participant_id_1"),
         diagnoses = Seq(
-          Diagnosis_ES(kfId = Some("diagnosis_11"), diagnosis = Some("Neuroblastoma Mondo"), mondoIdDiagnosis = Some("Neuroblastoma Mondo (MONDO:0005072)"), ncitIdDiagnosis = Some("Neuroblastoma NCIT (NCIT:C0475358)"), sourceTextDiagnosis = Some("Neuroblastoma source text")),
-          Diagnosis_ES(kfId = Some("diagnosis_12"), diagnosis = Some("Neuroblastoma NCIT"), ncitIdDiagnosis = Some("Neuroblastoma NCIT (NCIT:C0475358)"), sourceTextDiagnosis = Some("Neuroblastoma source text")),
-          Diagnosis_ES(kfId = Some("diagnosis_13"), diagnosis = Some("Neuroblastoma source text"), sourceTextDiagnosis = Some("Neuroblastoma source text"))
+          Diagnosis_ES(kfId = Some("diagnosis_11")),
+          Diagnosis_ES(kfId = Some("diagnosis_12"))
         )
       ),
       Participant_ES(kfId = Some("participant_id_2"),
-        diagnoses = Seq(
-          Diagnosis_ES(kfId = Some("diagnosis_21"), diagnosis = Some("Neuroblastoma Mondo"), mondoIdDiagnosis = Some("Neuroblastoma Mondo (MONDO:0005072)")),
-          Diagnosis_ES(kfId = Some("diagnosis_22"), diagnosis = Some("Ewing Sarcoma Mondo"), mondoIdDiagnosis = Some("Ewing Sarcoma Mondo (MONDO:0005073)"))
+        diagnoses = Seq(Diagnosis_ES(kfId = Some("diagnosis_21"))
         )
       ),
       Participant_ES(kfId = Some("participant_id_3"))
