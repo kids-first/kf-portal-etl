8870df32aad453dd333d4260c5595c628f66a6df
jecos <jecos.crsj@gmail.com>
2019-07-26 09:44:31 -0400

    Add diagnosis to biospecimens

--- kf-portal-etl-processors/src/main/scala/io/kf/etl/processors/common/step/impl/MergeBiospecimenPerParticipant.scala
+++ kf-portal-etl-processors/src/main/scala/io/kf/etl/processors/common/step/impl/MergeBiospecimenPerParticipant.scala
@@ -1,35 +1,28 @@
 package io.kf.etl.processors.common.step.impl
 
 import io.kf.etl.es.models.{Biospecimen_ES, Participant_ES}
-import io.kf.etl.external.dataservice.entity.EBiospecimen
+import io.kf.etl.external.dataservice.entity.{EBiospecimen, EBiospecimenDiagnosis, EDiagnosis}
 import io.kf.etl.external.hpo.OntologyTerm
 import io.kf.etl.processors.common.converter.PBEntityConverter
 import io.kf.etl.processors.common.step.StepExecutable
 import io.kf.etl.processors.common.step.context.StepContext
-import io.kf.etl.processors.common.step.impl.MergeBiospecimenPerParticipant.formatTerm
-import org.apache.spark.sql.Dataset
+import MergeBiospecimenPerParticipant._
+import org.apache.spark.sql.{Dataset, SparkSession}
 
 class MergeBiospecimenPerParticipant(override val ctx: StepContext) extends StepExecutable[Dataset[Participant_ES], Dataset[Participant_ES]] {
   override def process(participants: Dataset[Participant_ES]): Dataset[Participant_ES] = {
     import ctx.entityDataset.biospecimens
+    import ctx.entityDataset.biospecimenDiagnoses
+    import ctx.entityDataset.diagnoses
     import ctx.entityDataset.ontologyData.ncitTerms
     import ctx.spark.implicits._
 
-    val biospecimenJoinedWithNCIT: Dataset[EBiospecimen] = biospecimens
-      .joinWith(ncitTerms, biospecimens.col("ncitIdAnatomicalSite") === ncitTerms.col("id"), "left")
-      .map {
-        case (biospeciem, term) if term != null => biospeciem.copy(ncitIdAnatomicalSite = formatTerm(term))
-        case (biospecimen, _) => biospecimen
-      }
-      .joinWith(ncitTerms, $"ncitIdTissueType" === ncitTerms.col("id"), "left")
-      .map {
-        case (biospeciem, term) if term != null => biospeciem.copy(ncitIdTissueType = formatTerm(term))
-        case (biospeciem, _) => biospeciem
-      }
+    val biospecimenJoinedWithNCIT: Dataset[EBiospecimen] = enrichBiospecimenWithNcitTerms(biospecimens, ncitTerms)(ctx.spark)
+    val biospecimenJoinedWithDiagnosis = enrichBiospecimenWithDiagnoses(biospecimenJoinedWithNCIT, biospecimenDiagnoses, diagnoses)(ctx.spark)
 
     participants.joinWith(
-      biospecimenJoinedWithNCIT,
-      participants.col("kfId") === biospecimenJoinedWithNCIT.col("participantId"),
+      biospecimenJoinedWithDiagnosis,
+      participants.col("kfId") === biospecimenJoinedWithDiagnosis.col("participantId"),
       "left_outer"
     ).groupByKey { case (participant, _) => participant.kfId }
       .mapGroups((_, groupsIterator) => {
@@ -41,9 +34,37 @@ class MergeBiospecimenPerParticipant(override val ctx: StepContext) extends Step
         )
       })
   }
+
+
 }
 
 object MergeBiospecimenPerParticipant {
   def formatTerm(term: OntologyTerm) = Some(s"${term.name} (${term.id})")
+
+  private def enrichBiospecimenWithNcitTerms(biospecimens: Dataset[EBiospecimen], ncitTerms: Dataset[OntologyTerm])(spark: SparkSession) = {
+    import spark.implicits._
+    biospecimens
+      .joinWith(ncitTerms, biospecimens.col("ncitIdAnatomicalSite") === ncitTerms.col("id"), "left")
+      .map {
+        case (biospeciem, term) if term != null => biospeciem.copy(ncitIdAnatomicalSite = formatTerm(term))
+        case (biospecimen, _) => biospecimen
+      }
+      .joinWith(ncitTerms, $"ncitIdTissueType" === ncitTerms.col("id"), "left")
+      .map {
+        case (biospeciem, term) if term != null => biospeciem.copy(ncitIdTissueType = formatTerm(term))
+        case (biospeciem, _) => biospeciem
+      }
+  }
+
+  def enrichBiospecimenWithDiagnoses(biospecimens: Dataset[EBiospecimen], biospecimensDiagnoses: Dataset[EBiospecimenDiagnosis], diagnoses: Dataset[EDiagnosis])(spark: SparkSession): Dataset[EBiospecimen] = {
+    import spark.implicits._
+    val ds: Dataset[EBiospecimen] = biospecimens.joinWith(biospecimensDiagnoses, biospecimens("kfId") === biospecimensDiagnoses("biospecimenId"), joinType = "left")
+      .joinWith(diagnoses, diagnoses("kfId") === $"_2.diagnosisId", joinType = "left")
+      .map { case ((b, _), d) => (b, d) }
+      .groupByKey(_._1)
+      .mapGroups(
+        (biospecimen, iter) => biospecimen.copy(diagnoses = iter.collect { case (_, d) if d != null => d }.toSeq))
+    ds
+  }
 
 }
--- kf-portal-etl-processors/src/test/scala/io/kf/etl/processors/common/step/impl/MergeBiospecimenPerParticipantTest.scala
+++ kf-portal-etl-processors/src/test/scala/io/kf/etl/processors/common/step/impl/MergeBiospecimenPerParticipantTest.scala
@@ -1,7 +1,7 @@
 package io.kf.etl.processors.common.step.impl
 
-import io.kf.etl.es.models.{Biospecimen_ES, Participant_ES}
-import io.kf.etl.external.dataservice.entity.EBiospecimen
+import io.kf.etl.es.models.{Biospecimen_ES, Diagnosis_ES, Participant_ES}
+import io.kf.etl.external.dataservice.entity.{EBiospecimen, EBiospecimenDiagnosis, EDiagnosis}
 import io.kf.etl.external.hpo.OntologyTerm
 import io.kf.etl.processors.test.util.EntityUtil._
 import io.kf.etl.processors.test.util.StepContextUtil.buildContext
@@ -12,10 +12,12 @@ class MergeBiospecimenPerParticipantTest extends FlatSpec with Matchers with Wit
 
   import spark.implicits._
 
-  "process" should "join biospecimen and participant and enrich the ncid tissue tupe and anatomical site" in {
+  "process" should "join biospecimen and participant and enrich the ncid tissue type, anatomical site and diagnoses" in {
 
     val p1 = Participant_ES(kfId = Some("participant_id_1"))
     val bioSpecimen1 = EBiospecimen(kfId = Some("biospecimen_id_1"), participantId = Some("participant_id_1"), ncitIdAnatomicalSite = Some("NCIT:unknown"))
+    val diagnosis = EDiagnosis(kfId = Some("diagnosis_id_1"))
+    val biospecimenDiagnosis = EBiospecimenDiagnosis(kfId = Some("bd1"), diagnosisId = Some("diagnosis_id_1"), biospecimenId = Some("biospecimen_id_1"))
 
     val p2 = Participant_ES(kfId = Some("participant_id_2"))
     val bioSpecimen21 = EBiospecimen(kfId = Some("biospecimen_id_21"), participantId = Some("participant_id_2"), ncitIdAnatomicalSite = Some("NCIT:C12438"), ncitIdTissueType = Some("NCIT:C14165"))
@@ -36,7 +38,9 @@ class MergeBiospecimenPerParticipantTest extends FlatSpec with Matchers with Wit
     )
     val entityDataset = buildEntityDataSet(
       biospecimens = Seq(bioSpecimen1, bioSpecimen21, bioSpecimen22, bioSpecimen3),
-      ontologyData = Some(ontologiesDataset)
+      ontologyData = Some(ontologiesDataset),
+      diagnoses = Seq(diagnosis),
+      biospecimenDiagnoses = Seq(biospecimenDiagnosis)
     )
 
     val mergeBiospecimen = new MergeBiospecimenPerParticipant(ctx = buildContext(entityDataset))
@@ -45,15 +49,46 @@ class MergeBiospecimenPerParticipantTest extends FlatSpec with Matchers with Wit
     val result = mergeBiospecimen.process(participantsDataset).collect()
     result should contain theSameElementsAs Seq(
       Participant_ES(kfId = Some("participant_id_1"),
-        biospecimens = Seq(Biospecimen_ES(kfId = Some("biospecimen_id_1"), ncitIdAnatomicalSite = Some("NCIT:unknown")))
+        biospecimens = Seq(Biospecimen_ES(kfId = Some("biospecimen_id_1"), ncitIdAnatomicalSite = Some("NCIT:unknown"), diagnoses = Seq(Diagnosis_ES(kfId = Some("diagnosis_id_1")))))
       ),
       Participant_ES(kfId = Some("participant_id_2"),
         biospecimens = Seq(
-          Biospecimen_ES(kfId = Some("biospecimen_id_21"), ncitIdAnatomicalSite = Some("Central nervous system (NCIT:C12438)"), ncitIdTissueType = Some("Normal (NCIT:C14165)")),
-          Biospecimen_ES(kfId = Some("biospecimen_id_22")))),
+          Biospecimen_ES(kfId = Some("biospecimen_id_22")),
+          Biospecimen_ES(kfId = Some("biospecimen_id_21"), ncitIdAnatomicalSite = Some("Central nervous system (NCIT:C12438)"), ncitIdTissueType = Some("Normal (NCIT:C14165)"))
+        )),
       Participant_ES(kfId = Some("participant_id_3"))
     )
 
 
   }
+
+  "enrichBiospecimenWithDiagnoses" should "join biospecimens with diagnoses" in {
+    val bioSpecimens = Seq(
+      EBiospecimen(kfId = Some("biospecimen_id_1")),
+      EBiospecimen(kfId = Some("biospecimen_id_2")),
+      EBiospecimen(kfId = Some("biospecimen_id_3"))
+
+    )
+
+    val diagnoses = Seq(
+      EDiagnosis(kfId = Some("diagnosis_id_1")),
+      EDiagnosis(kfId = Some("diagnosis_id_2")),
+      EDiagnosis(kfId = Some("diagnosis_id_3"))
+    )
+
+    val biospecimensDiagnosis = Seq(
+      EBiospecimenDiagnosis(kfId = Some("bd1"), diagnosisId = Some("diagnosis_id_1"), biospecimenId = Some("biospecimen_id_1")),
+      EBiospecimenDiagnosis(kfId = Some("bd1"), diagnosisId = Some("diagnosis_id_2"), biospecimenId = Some("biospecimen_id_1")),
+      EBiospecimenDiagnosis(kfId = Some("bd1"), diagnosisId = Some("diagnosis_id_3"), biospecimenId = Some("biospecimen_id_2"))
+    )
+
+    val result = MergeBiospecimenPerParticipant.enrichBiospecimenWithDiagnoses(bioSpecimens.toDS(), biospecimensDiagnosis.toDS(), diagnoses.toDS())(spark).collect()
+    result should contain theSameElementsAs Seq(
+      EBiospecimen(kfId = Some("biospecimen_id_1"), diagnoses = Seq(EDiagnosis(kfId = Some("diagnosis_id_2")), EDiagnosis(kfId = Some("diagnosis_id_1")))),
+      EBiospecimen(kfId = Some("biospecimen_id_2"), diagnoses = Seq(EDiagnosis(kfId = Some("diagnosis_id_3")))),
+      EBiospecimen(kfId = Some("biospecimen_id_3"), diagnoses = Nil)
+    )
+  }
+
+
 }
