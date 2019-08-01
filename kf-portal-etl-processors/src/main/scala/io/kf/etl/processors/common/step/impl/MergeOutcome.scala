32fccce2ea795506fd78dedbe465fa1b73abe517
jecos <jecos.crsj@gmail.com>
2019-08-01 08:37:52 -0400

    Keep last outcome observed

--- kf-portal-etl-processors/src/main/scala/io/kf/etl/processors/common/step/impl/MergeOutcome.scala
+++ kf-portal-etl-processors/src/main/scala/io/kf/etl/processors/common/step/impl/MergeOutcome.scala
@@ -1,6 +1,7 @@
 package io.kf.etl.processors.common.step.impl
 
-import io.kf.etl.es.models.{Outcome_ES, Participant_ES}
+import io.kf.etl.es.models.Participant_ES
+import io.kf.etl.external.dataservice.entity.EOutcome
 import io.kf.etl.processors.common.converter.PBEntityConverter
 import io.kf.etl.processors.common.step.StepExecutable
 import io.kf.etl.processors.common.step.context.StepContext
@@ -13,16 +14,29 @@ class MergeOutcome(override val ctx: StepContext) extends StepExecutable[Dataset
       ctx.entityDataset.outcomes,
       participants.col("kfId") === ctx.entityDataset.outcomes.col("participantId"),
       "left_outer"
-    ).map(tuple => {
-      val outcome = Option(tuple._2) match {
-        case Some(outcome) => {
-          Some(PBEntityConverter.EOutcomeToOutcomeES(outcome))
-        }
-        case None => Some(Outcome_ES())
-      }
+    )
+      .as[(Participant_ES, Option[EOutcome])]
+      .groupByKey { case (participant, _) => participant.kfId }
+      .mapGroups((_, groupsIterator) => {
+        val groups = groupsIterator.toSeq
+        val participant = groups.head._1
 
-      tuple._1.copy(outcome = outcome)
 
-    })
+        val outcomes = groups.flatMap(_._2)
+        if (outcomes.nonEmpty) {
+          val deceasedOutcome: Option[EOutcome] = outcomes.find(o => o.vitalStatus.contains("Deceased"))
+          deceasedOutcome match {
+            case None =>
+              val lastOutcome = outcomes.maxBy(_.ageAtEventDays)
+              participant.copy(
+                outcome = Some(PBEntityConverter.EOutcomeToOutcomeES(lastOutcome))
+              )
+            case Some(o) => participant.copy(outcome = Some(PBEntityConverter.EOutcomeToOutcomeES(o)))
+          }
+
+        } else
+          participant
+      })
+
   }
 }
