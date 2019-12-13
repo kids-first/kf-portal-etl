e700fe4668fd3a754e3e45db27478030a74614f2
Adrian Paul <adipaul1981@yahoo.ca>
2019-12-13 16:43:08 -0500

    :construction: Wip - testing

---
+++ src/main/scala/io/kf/etl/models/internal/BiospecimenCombinedES_GenomicFileES.scala
@@ -0,0 +1,5 @@
+package io.kf.etl.models.internal
+
+import io.kf.etl.models.es.{BiospecimenCombined_ES, GenomicFile_ES}
+
+case class BiospecimenCombinedES_GenomicFileES(bio : BiospecimenCombined_ES, genomicFile : GenomicFile_ES)
