0b9b02c508a97cb3561b5046188f65ab210b88a6
Adrian Paul <adipaul1981@yahoo.ca>
2019-12-20 08:33:30 -0500

    :white_check_mark: Update file centric test

--- src/main/scala/io/kf/etl/models/es/SequencingExperiment_ES.scala
+++ src/main/scala/io/kf/etl/models/es/SequencingExperiment_ES.scala
@@ -1,19 +1,19 @@
 package io.kf.etl.models.es
 
 final case class SequencingExperiment_ES(
-                                          kf_id: scala.Option[String] = None,
-                                          experiment_date: scala.Option[String] = None,
-                                          experiment_strategy: scala.Option[String] = None,
-                                          sequencing_center_id: scala.Option[String] = None,
-                                          library_name: scala.Option[String] = None,
-                                          library_strand: scala.Option[String] = None,
-                                          is_paired_end: scala.Option[Boolean] = None,
-                                          platform: scala.Option[String] = None,
-                                          instrument_model: scala.Option[String] = None,
-                                          max_insert_size: scala.Option[Long] = None,
-                                          mean_insert_size: scala.Option[Double] = None,
-                                          mean_depth: scala.Option[Double] = None,
-                                          total_reads: scala.Option[Long] = None,
-                                          mean_read_length: scala.Option[Double] = None,
-                                          external_id: scala.Option[String] = None
+                                          kf_id: Option[String] = None,
+                                          experiment_date: Option[String] = None,
+                                          experiment_strategy: Option[String] = None,
+                                          sequencing_center_id: Option[String] = None,
+                                          library_name: Option[String] = None,
+                                          library_strand: Option[String] = None,
+                                          is_paired_end: Option[Boolean] = None,
+                                          platform: Option[String] = None,
+                                          instrument_model: Option[String] = None,
+                                          max_insert_size: Option[Long] = None,
+                                          mean_insert_size: Option[Double] = None,
+                                          mean_depth: Option[Double] = None,
+                                          total_reads: Option[Long] = None,
+                                          mean_read_length: Option[Double] = None,
+                                          external_id: Option[String] = None
                                         )
