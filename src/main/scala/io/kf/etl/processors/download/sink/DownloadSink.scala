7119704f7fa4eac59bd6573c59ebf7409e475886
jecos <jecos.crsj@gmail.com>
2020-05-28 08:21:53 -0400

    Download biospecimens_diagnoses table as parquet file

--- src/main/scala/io/kf/etl/processors/download/sink/DownloadSink.scala
+++ src/main/scala/io/kf/etl/processors/download/sink/DownloadSink.scala
@@ -14,6 +14,7 @@ object DownloadSink {
     data.families.write.mode(SaveMode.Overwrite).parquet(s"$sinkPath/families/$studyId")
     data.biospecimens.write.mode(SaveMode.Overwrite).parquet(s"$sinkPath/biospecimens/$studyId")
     data.diagnoses.write.mode(SaveMode.Overwrite).parquet(s"$sinkPath/diagnoses/$studyId")
+    data.biospecimenDiagnoses.write.mode(SaveMode.Overwrite).parquet(s"$sinkPath/biospecimens_diagnoses/$studyId")
     data.familyRelationships.write.mode(SaveMode.Overwrite).parquet(s"$sinkPath/family_relationships/$studyId")
     data.genomicFiles.write.mode(SaveMode.Overwrite).parquet(s"$sinkPath/genomic_files/$studyId")
     data.sequencingExperiments.write.mode(SaveMode.Overwrite).parquet(s"$sinkPath/sequencing_experiments/$studyId")
