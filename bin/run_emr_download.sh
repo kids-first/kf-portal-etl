dd6c1a1f4ddcb668d93acbad1b139d144ea94035
jecos <jcostanza@ferlab.bio>
2021-01-15 11:42:11 -0500

    Modification to shell script due to account migration

--- bin/run_emr_download.sh
+++ bin/run_emr_download.sh
@@ -15,14 +15,14 @@ steps=$(cat <<EOF
     },
     {
         "Name":"Upload duocodes to s3",
-        "Args":["aws", "s3", "cp", "/home/hadoop/duo.csv", "s3://kf-qa-etl-bucket/ontologies/duo_codes/duo.csv"],
+        "Args":["aws", "s3", "cp", "/home/hadoop/duo.csv", "s3://kf-strides-variant-parquet-prd/ontologies/raw/duo_codes/duo.csv"],
         "ActionOnFailure":"TERMINATE_CLUSTER",
         "Type":"CUSTOM_JAR",
         "Jar":"command-runner.jar"
     },
     {
         "Name":"Cpy congi file",
-        "Args":["aws","s3","cp","s3://kf-variant-parquet-prd/jobs/conf/kf-etl.conf", "/home/hadoop"],
+        "Args":["aws","s3","cp","s3://kf-strides-variant-parquet-prd/jobs/conf/kf-etl.conf", "/home/hadoop"],
         "ActionOnFailure":"TERMINATE_CLUSTER",
         "Type":"CUSTOM_JAR",
         "Jar":"command-runner.jar"
@@ -31,7 +31,7 @@ steps=$(cat <<EOF
       "Args":[
           "spark-submit","--deploy-mode","client",
           "--class","io.kf.etl.ETLDownloadOnlyMain",
-          "s3a://kf-variant-parquet-prd/jobs/kf-portal-etl.jar",
+          "s3a://kf-strides-variant-parquet-prd/jobs/kf-portal-etl.jar",
           "${study_ids}"
       ],
       "Type":"CUSTOM_JAR",
@@ -46,11 +46,11 @@ EOF
 instance_groups="[{\"InstanceCount\":1,\"InstanceGroupType\":\"CORE\",\"InstanceType\":\"${instance_type}\",\"Name\":\"Core - 2\"},{\"InstanceCount\":1,\"EbsConfiguration\":{\"EbsBlockDeviceConfigs\":[{\"VolumeSpecification\":{\"SizeInGB\":32,\"VolumeType\":\"gp2\"},\"VolumesPerInstance\":2}]},\"InstanceGroupType\":\"MASTER\",\"InstanceType\":\"m5.xlarge\",\"Name\":\"Master - 1\"}]"
 
 aws emr create-cluster --applications Name=Hadoop Name=Spark \
---ec2-attributes '{"KeyName":"flintrock","AdditionalSlaveSecurityGroups":["sg-059bf5fe80ff903be"],"InstanceProfile":"EMR_EC2_DefaultRole","ServiceAccessSecurityGroup":"sg-0487486fd3d67c14e","SubnetId":"subnet-a756a3ed","EmrManagedSlaveSecurityGroup":"sg-0807b9c40bb37be85","EmrManagedMasterSecurityGroup":"sg-012f30e67b51b6f4d","AdditionalMasterSecurityGroups":["sg-059bf5fe80ff903be"]}' \
---service-role EMR_DefaultRole \
+--ec2-attributes '{"KeyName":"flintrock","InstanceProfile":"kf-variant-emr-ec2-prd-profile","ServiceAccessSecurityGroup":"sg-0587a1d20e24f4104","SubnetId":"subnet-00aab84919d5a44e2","EmrManagedSlaveSecurityGroup":"sg-0dc6b48e674070821","EmrManagedMasterSecurityGroup":"sg-0a31895d33d1643da"}' \
+--service-role kf-variant-emr-prd-role \
 --enable-debugging \
 --release-label emr-5.19.0 \
---log-uri 's3n://aws-logs-538745987955-us-east-1/elasticmapreduce/' \
+  --log-uri 's3n://kf-strides-variant-parquet-prd/jobs/elasticmapreduce/' \
 --steps "${steps}" \
 --name "Download clinical data - Studies ${study_ids} - Release ${release_id}" \
 --instance-groups "${instance_groups}" \
