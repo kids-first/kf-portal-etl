9d54e48c46a004726c8fdd0a960816227873edc2
Adrian Paul <adipaul1981@yahoo.ca>
2021-01-27 12:01:40 -0500

    write dataset to json file

--- src/main/scala/io/kf/etl/processors/participantcommon/transform/step/WriteJsonSink.scala
+++ src/main/scala/io/kf/etl/processors/participantcommon/transform/step/WriteJsonSink.scala
@@ -8,8 +8,7 @@ object WriteJsonSink {
   def apply[T](processorName: String)(step: String)(input: Dataset[T])(implicit config: Config, spark: SparkSession): Dataset[T] = {
     if (!config.getBoolean(s"processors.$processorName.write_intermediate_data")) {
       input
-    }
-    else {
+    } else {
       val cached = input.cache()
       val dataPath = config.getString(s"processors.$processorName.data_path")
       val stepDataPath = s"$dataPath/steps/$step"
@@ -17,5 +16,11 @@ object WriteJsonSink {
       cached.write.mode(SaveMode.Overwrite).json(stepDataPath)
       cached
     }
+  }
+
+  def apply[T](sinkPath: String)(input: Dataset[T])(implicit spark: SparkSession): Dataset[T] = {
+    val cached = input.cache()
+    input.write.mode(SaveMode.Overwrite).json(sinkPath)
+    cached
   }
 }
--- src/main/scala/io/kf/etl/processors/tojson/JsonOutputProcessor.scala
+++ src/main/scala/io/kf/etl/processors/tojson/JsonOutputProcessor.scala
@@ -1,42 +1,18 @@
 package io.kf.etl.processors.tojson
 
 import com.typesafe.config.Config
-import io.kf.etl.context.DefaultContext
-import io.kf.etl.processors.index.mapping.MappingFiles
-import org.apache.spark.sql.Dataset
-import org.elasticsearch.spark.sql._
-import play.api.libs.ws.DefaultBodyWritables.writeableOf_String
+import io.kf.etl.common.Constants.JSON_OUTPUT_FILES
+import io.kf.etl.processors.participantcommon.transform.step.WriteJsonSink
+import org.apache.spark.sql.{Dataset, SparkSession}
 import play.api.libs.ws.StandaloneWSClient
-
-import scala.concurrent.Await
-import scala.concurrent.duration.DurationInt
 
 
 object JsonOutputProcessor {
-  def apply[T](indexType: String, studyId: String, releaseId: String, dataset: Dataset[T])(implicit wsClient: StandaloneWSClient, config: Config): Unit = {
-    val indexName = getIndexName(indexType, studyId, releaseId)
-    createMapping(indexName, indexType)
-    dataset.toDF().saveToEs(s"$indexName/$indexType", Map("es.mapping.id" -> "kf_id"))
+  def apply[T](datasets: Map[String, Dataset[T]])(implicit wsClient: StandaloneWSClient, config: Config, spark: SparkSession): Unit = {
 
-  }
-
-  def getIndexName(indexType: String, studyId: String, releaseId: String): String = {
-    s"${indexType}_${studyId}_$releaseId".toLowerCase
-  }
-
-
-  private def createMapping(indexName: String, indexType: String)(implicit wsClient: StandaloneWSClient, config: Config): Unit = {
-    val content = MappingFiles.getMapping(indexType)
-
-    val elasticSearchUrl = DefaultContext.elasticSearchUrl(config)
-    val response = Await.result(wsClient
-      .url(s"$elasticSearchUrl/$indexName")
-      .withHttpHeaders("Content-Type" -> "application/json")
-      .put(content), 30 seconds
+    datasets.foreach(d =>
+      WriteJsonSink[T](config.getString(JSON_OUTPUT_FILES))(d._2)
     )
-
-    if (response.status != 200) {
-      throw new IllegalStateException(s"Impossible to create index :${response}")
-    }
   }
+
 }
