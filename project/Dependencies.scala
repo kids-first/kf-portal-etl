f5d7b551321a7c7c795fe9680e3c631c30d02c75
Adrian Paul <adipaul1981@yahoo.ca>
2021-10-04 11:26:46 -0400

    update elasticspark

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val spark_sql = "org.apache.spark" %% "spark-sql" % spark_version % Provided
   val typesafe_config = "com.typesafe" % "config" % "1.3.2"
   val asyncHttp = "org.asynchttpclient" % "async-http-client" % "2.4.5"
-  val es_spark = "org.elasticsearch" %% "elasticsearch-spark-20" % "6.1.3"
+  val es_spark = "org.elasticsearch" %% "elasticsearch-spark-20" % "7.15.0"
   val scalatest = "org.scalatest" %% "scalatest" % "3.0.4"
   val json4s = "org.json4s" %% "json4s-jackson" % "3.2.11"
 }
